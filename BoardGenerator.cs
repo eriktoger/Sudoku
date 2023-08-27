f2528322aa999ce3a5c46f296346bbbf7d5266af
Erik Töger <erik.toger@gmail.com>
2023-08-27 11:07:11 +0200

    Swap rows in each segment

--- BoardGenerator.cs
+++ BoardGenerator.cs
@@ -6,12 +6,28 @@
         private List<int> _initialRow;
         private readonly Random _rand;
         private readonly List<int> _lineShufflers;
+        private readonly List<List<int>> _lineSwappers;
 
         public BoardGenerator()
         {
             _initialRow = Enumerable.Range(1, 9).ToList();
             _rand = new Random();
             _lineShufflers = new List<int>{ 3, 3, 1, 3, 3, 1, 3, 3 };
+            _lineSwappers = new List< List<int>> {
+                new List<int> { 0, 1}, new List<int> { 0, 2 }, new List<int> { 1, 2 },
+                new List<int> { 3, 4 }, new List<int> { 3, 5 }, new List<int> { 4, 5 },
+                new List<int> { 6, 7 }, new List<int> { 6, 8 },new List<int> { 7, 8} };
+        }
+
+        private void SwapRows(List<List<int>>board,int rowIndex1,int rowIndex2)
+        {
+            if(_rand.Next() % 2 == 0)
+            {
+                var temp = board[rowIndex1];
+                board[rowIndex1] = board[rowIndex2];
+                board[rowIndex2] = temp;
+            }
+
         }
 
        
@@ -32,6 +48,11 @@
                 currentRow = second;
                 board.Add(currentRow);
 
+            }
+
+            foreach( var lineSwapper in _lineSwappers)
+            {
+                 SwapRows(board,lineSwapper[0], lineSwapper[1]);
             }
 
             return board;
